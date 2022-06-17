8de081a546ad6205778091a103e39d3e06e0d864
Jerome David <programingjd@gmail.com>
2022-06-18 01:04:34 +0200

    -

--- q85.mjs
+++ q85.mjs
@@ -1,34 +1,79 @@
 const i2c=[...new TextEncoder().encode('!&()*+,-.0123456789:;=?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{|}~')];
 const c2i=[];
 i2c.forEach((it,i)=>c2i[it-33]=i);
-const five=[0,1,2,3,4];
+/**
+ * @typedef {Int8Array|Uint8Array|Uint8ClampedArray|Int16Array|Uint16Array|Int32Array|
+ *           Uint32Array|Float32Array|Float64Array|BigInt64Array|BigUint64Array} TypedArray
+ */
+/**
+ * @template T
+ * @template {Array<T>|TypedArray} A
+ * @param {A} arr
+ * @param {number} n
+ * @return {Generator<A,void,*>}
+ */
 function* chunks(arr, n) {
   if(n===0) yield arr;
   else for(let i=0; i<arr.length; i+=n) yield arr.slice(i,i+n);
 }
-const buffer=data=>{
+/**
+ * @param {string|TypedArray} data
+ * @return {Uint8Array}
+ */
+const asBytes=data=>{
   if(typeof data==='string') data=new TextEncoder().encode(data);
-  if(ArrayBuffer.isView(data)) data=data.buffer;
-  if(data instanceof ArrayBuffer) return data;
-  throw new Error('Expected string, typed array or array buffer.');
+  if(data instanceof Uint8Array) return data;
+  if(ArrayBuffer.isView(data)) return new Uint8Array(data.buffer,0,data.byteLength);
+  throw new Error('Expected string or typed array.');
 }
+/**
+ * @param {string|TypedArray} data
+ * @return {string}
+ */
 const enc85=data=>{
-  let buf=buffer(data);
-  if(buf.byteLength%4!==0){
-    const enlarged=new Uint8Array(Math.ceil(buf.byteLength/4)*4);
-    enlarged.set(new Uint8Array(buf));
-    buf=enlarged.buffer;
-  }
-  const res=[...new Uint32Array(buf)].flatMap(it=>five.map(_=>{
-    const c=i2c[it%85];
-    it=Math.trunc(it/85);
-    return c;
-  }));
-  return new TextDecoder().decode(Uint8Array.from(res)).replace(/!+$/,'');
-};
-const dec85=data=>{
-  const buf=buffer(data);
-  const u32s=[...chunks(new Uint8Array(buf),5)].map(it=>it.reverse().reduce((prev,it)=>prev*85+c2i[it-33],0));
-  return new TextDecoder().decode(new Uint32Array(u32s)).replace(/\u0000+$/,'');
+  const bytes=asBytes(data);
+  const suffixed=new Uint8Array(bytes.byteLength+1);
+  suffixed.set(bytes);
+  suffixed.set([1],bytes.byteLength);
+  /** @type {Array<number>} */
+  const chars=[...chunks(suffixed,4)].flatMap(chunk=>{
+    let u32=chunk.reverse().reduce((prev,it)=>prev*256+it,0);
+    const chars=[];
+    for(let i=0;i<5;++i){
+      chars.push(i2c[u32%85]);
+      u32=Math.trunc(u32/85);
+    }
+    if(chunk.length===4) return chars;
+    for(let i=0;i<5;++i){
+      if(chars[chars.length-1]===33) chars.pop();
+      else break;
+    }
+    return chars;
+  });
+  return new TextDecoder().decode(Uint8Array.from(chars));
 }
+/**
+ * @param {string} data
+ * @return {Uint8Array}
+ */
+const dec85=data=>{
+  /** @type {Array<number>} */
+  const bytes=[...chunks(asBytes(data),5)].flatMap(chunk=>{
+    let u32=chunk.reverse().reduce((prev,it)=>prev*85+c2i[it-33],0);
+    const bytes=[];
+    for(let i=0;i<4;++i){
+      bytes.push(u32%256);
+      u32=Math.trunc(u32/256);
+    }
+    if(chunk.length===5) return bytes;
+    for(let i=0;i<4;++i){
+      if(bytes[bytes.length-1]===0) bytes.pop();
+      else break;
+    }
+    return bytes;
+  });
+  bytes.pop();
+  return Uint8Array.from(bytes);
+}
+
 export {enc85,dec85};
--- tests/q85_test.js
+++ tests/q85_test.js
@@ -1,25 +1,31 @@
 import {assertEquals} from "https://deno.land/std/testing/asserts.ts";
-import {dec85,enc85} from '../q85.mjs';
+import {dec85 as dec85_,enc85} from '../q85.mjs';
+const dec85=data=>new TextDecoder().decode(dec85_(data));
+
 
 Deno.test("Encode/Decode ascii",()=>{
   const text='Testing data.';
   const encoded=enc85(text);
-  assertEquals(encoded,'v&CMN1[;L1pyFDHW');
+  assertEquals(encoded,'v&CMN1[;L1pyFDHX)');
   const decoded=dec85(encoded);
   assertEquals(decoded,text);
 });
 Deno.test("Encode/Decode unicode",()=>{
   const text='てスト 🤔';
   const encoded=enc85(text);
-  assertEquals(encoded,'oK45sxT,JSc}DK]c?+');
+  assertEquals(encoded,'oK45sxT,JSc}DK]dE5');
   const decoded=dec85(encoded);
   assertEquals(decoded,text);
 });
 Deno.test("Encode/Decode binary",()=>{
-  const text='!4 てスト 🤔';
-  const encoded=enc85(text);
-  assertEquals(encoded,'MVfD&oK45sxT,JSc}DK]fQI');
+  const binary=String.fromCharCode(
+    0x21,0x54,0x00,0x04,0x3c,0x64,0x69,0x76,0x3e,0x54,0x65,0x73,0x74,0x20,0x64,0x61,
+    0x74,0x61,0x2e,0x3c,0x2f,0x64,0x69,0x76,0x3e,0x0a,0x03
+  );
+  assertEquals(binary.length,27);
+  const encoded=enc85(binary);
+  assertEquals(encoded,'MKCA&TRt*O?DR-N+T^BHj+-F:GRt*Ovc`D!');
   const decoded=dec85(encoded);
-  assertEquals(decoded,text);
+  assertEquals(decoded,binary);
 });
 
