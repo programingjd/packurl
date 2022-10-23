f8f727a7d55f42d514eadcda956689228856822e
Jerome David <programingjd@gmail.com>
2022-10-23 14:06:22 +0200

    -

--- server2/src/cache.rs
+++ server2/src/cache.rs
@@ -1,4 +1,4 @@
-// use lazy_static::lazy_static;
+use lazy_static::lazy_static;
 use rustls::sign::CertifiedKey;
 use std::borrow::Borrow;
 use std::cell::{Ref, RefCell};
@@ -8,13 +8,12 @@ use std::ops::{Deref, DerefMut};
 use std::rc::Rc;
 use std::sync::RwLock;
 
-const ACCOUNT_KEYS: RwLock<Option<Vec<u8>>> = RwLock::new(None);
-const ACCOUNT_KID: RwLock<Option<Vec<u8>>> = RwLock::new(None);
-// lazy_static! {
-//     static ref CHALLENGE_KEY: Arc<RefCell<HashMap<String, CertifiedKey>>> =
-//         Arc::new(RefCell::new(HashMap::new()));
-// }
-const CHALLENGE_KEY: RwLock<Option<HashMap<String, CertifiedKey>>> = RwLock::new(None);
+lazy_static! {
+    static ref ACCOUNT_KEYS: RwLock<Option<Vec<u8>>> = RwLock::new(None);
+    static ref ACCOUNT_KID: RwLock<Option<Vec<u8>>> = RwLock::new(None);
+    static ref CHALLENGE_KEY: RwLock<Option<HashMap<String, CertifiedKey>>> = RwLock::new(None);
+}
+//const CHALLENGE_KEY: RwLock<Option<HashMap<String, CertifiedKey>>> = RwLock::new(None);
 
 pub async fn restore_account_keys() -> Option<Vec<u8>> {
     ACCOUNT_KEYS.read().map_or(None, |it| it.clone())
@@ -41,7 +40,7 @@ pub async fn backup_account_kid(bytes: &[u8]) -> Result<()> {
 }
 pub fn set_challenge_key(domain: &str, key: CertifiedKey) -> Result<()> {
     if let Ok(mut lock) = CHALLENGE_KEY.write() {
-        let mut option = lock.as_mut();
+        let option = lock.as_mut();
         if let Some(map) = option {
             println!("Inserting into existing map.");
             map.insert(domain.to_string(), key);
